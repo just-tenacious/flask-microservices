0ddfb99b0418cfb680b15b963dae89105f466957
just-tenacious <tshraddha1505@gmail.com>
2025-09-13 15:46:53 +0530

    Added  a new route

--- web-service/app.py
+++ web-service/app.py
@@ -15,5 +15,9 @@ def home():
 def about():
 	return "Web Service in Microservices Demo!"
 
+@app.route('/newRoute')
+def newRoute():
+	return "This is a new route!"
+
 if __name__=="__main__":
 	app.run(host='0.0.0.0',port=5000)
